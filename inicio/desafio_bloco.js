b4c57cdf247d9a6f241a7d2f202ed579bd7120d5
henrico0souza <henrico.souzacr7@gmail.com>
2026-02-26 12:15:11 -0300

    inicio.js

---
+++ inicio/desafio_bloco.js
@@ -0,0 +1,12 @@
+/*
+    desafio: criar dois blocos de codigo.
+    onde o preimeiro deve imprimir na tela os
+    numeros pares de 0 a 10 e o segundo
+    bloco de imprimir na tela impares
+*/
+
+{
+    console.log("numeros pares: ");
+   console.log(2);
+cnsole.log(2);
+}
