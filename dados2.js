e0e294c60ad5ad60935a418f0815bb48d8f6e67e
henrico0souza <henrico.souzacr7@gmail.com>
2026-02-27 11:47:17 -0300

    Tipos de dados em js

---
+++ dados2.js
@@ -0,0 +1,13 @@
+//console.log(19.9 * 0.6);
+
+var preco = 19.9;
+var desconto = 0.6;
+var precoComDesconto = preco * (1 - desconto);
+//console.log(precoComDescomto);
+
+let nome = "caderno";
+let categoria = "papelaria";
+ console.log("produto: " + nome +
+     ", categoria: " + categoria +
+      ", preço: " +preco +
+      ", Desconto:" + Desconto);
