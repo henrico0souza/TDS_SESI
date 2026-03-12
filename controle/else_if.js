0cd44f65190409393265e2c8da36b30170fad3fd
henrico0souza <henrico.souzacr7@gmail.com>
2026-03-12 12:12:42 -0300

    estruturas de controle

---
+++ controle/else_if.js
@@ -0,0 +1,15 @@
+//desafio
+
+let n1 = 8;
+let n2 = 7; 
+let media = 7.5;
+
+media = (n1 + n2) /2;
+
+if (media >= 7.5){
+    console.log("Aprovado");
+}else if(media <= 7.5 && media >= 5 ){
+    console.log("Recuperaçao");
+}else{
+    console.log("Reprovado");
+}
---
+++ controle/template_strig.js
@@ -0,0 +1,6 @@
+let nome = "henrico gabriel";
+let idade = 15
+// imprimir no terminal o nome e a idade
+// junto vom a frase.
+console.log("meu nome é" + nome + "e tenho" + idade + "anos de idade.");
+console.log(`meu nome é $ {nome} e tenho ${idade} anos de idade.`);
