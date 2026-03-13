0421cbc97095d781d275cc300eb3a36323292f8d
henrico0souza <henrico.souzacr7@gmail.com>
2026-03-13 12:15:53 -0300

    array

---
+++ array/array2.js
@@ -0,0 +1,11 @@
+const nomes = []
+
+nomes [0] ="henrico"
+nomes [1] = "joão"
+nomes [2] = "angelo"
+nomes [3] = "everson"
+nomes [4] = "alex"
+console.log(nomes.length); // quantidades de indice dentro do array
+
+nomes[5] = "kauan"
+console.log(nomes);
