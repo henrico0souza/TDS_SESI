74b7350ff3fca79d3dede33f850a52b36f517dad
henrico0souza <henrico.souzacr7@gmail.com>
2026-04-10 09:41:42 -0300

    atividade

--- atividades/atividade12.js
+++ atividades/atividade12.js
@@ -1,14 +1,8 @@
-const clienteNome = {
-    nome : "Henrico",
-}
+const clienteNome = "Henrico"
 
- var clienteidade = {
-    idade : "15", 
-}
+var clienteidade = 15
 
-var possuicupom = {
-    cupom : true
-}
+var possuicupom = true
 
 console.log(clienteNome);
 console.log(clienteidade);
@@ -16,18 +10,7 @@ console.log(possuicupom);
 
 const componentes = ["culer", "monitor", "mouse", "placa de video, Memoria Ram"];
  
-componentes.shift(componentes[0]);
-componentes.push("CPU");
-
-
- console.log(componentes[0]);
- console.log(componentes[1]);
- console.log(componentes[2]);
- console.log(componentes[3]);
- console.log(componentes);
-
  function processarvenda (valorfinal, cupomativo, quantidadedeitens ) {
     valorfinal > 500|| cupomativo * 0.20|| quantidadedeitens >= 3
 }
 
-console.log(processarvenda(3000, 0.20, 3));
